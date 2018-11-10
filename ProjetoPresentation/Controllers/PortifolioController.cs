e37b29df5243c638d7833ad51184232c59e98e9c
Vanderlei Campos <38666926+Vander27@users.noreply.github.com>
2018-11-10 21:51:19 -0200

    Initial Commit

---
+++ ProjetoPresentation/Controllers/PortifolioController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoPresentation.Controllers
+{
+    public class PortifolioController : Controller
+    {
+        // GET: Portifolio/Portifolio
+        public ActionResult Portifolio()
+        {
+            return View();
+        }
+
+        // GET: Portifolio/Indice
+        public ActionResult Indice()
+        {
+            return View();
+        }
+
+        // GET: Portifolio/Contato
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        // GET: Portifolio/Servico
+        public ActionResult Servico()
+        {
+            return View();
+        }
+
+        // GET: Portifolio/Sobre
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        // GET: Portifolio/Curriculo
+        public ActionResult Curriculo()
+        {
+            return View();
+        }
+
+       
+    }
+}
+
